bb510f058dd9dbb7d574368dc68337e25c30764f
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2023-03-30 15:13:32 -0400

    pretty printing and upload current file

--- src/main/java/com/example/ExamplePlugin.java
+++ src/main/java/com/example/ExamplePlugin.java
@@ -3,28 +3,29 @@ package com.example;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.inject.Provides;
-import javax.inject.Inject;
-import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
-import net.runelite.api.events.PlayerChanged;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.PlayerChanged;
 import net.runelite.api.events.PlayerSpawned;
 import net.runelite.api.kit.KitType;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
-@Slf4j
 @PluginDescriptor(
 	name = "Example"
 )
 public class ExamplePlugin extends Plugin
 {
+	private static final Logger log = LoggerFactory.getLogger(ExamplePlugin.class);
 	@Inject
 	private Client client;
 
@@ -38,10 +39,14 @@ public class ExamplePlugin extends Plugin
 		int itemId = player.getPlayerComposition().getEquipmentId(KitType.WEAPON);
 
 		if(itemId >-1) {
-			if(!client.getWorldType().contains(WorldType.MEMBERS)) {//skip animations from member items on f2p worlds.
+			if(!client.getWorldType().contains(WorldType.MEMBERS)) {
 				ItemComposition definition = client.getItemDefinition(itemId);
-				if (definition.isMembers())
+				if (definition.isMembers()) {
+					//skip animations from member items on f2p worlds.
+					//this is because on f2p worlds member items use the same set of animations.
+					//"stand":808,"walk":819,"run":824,"walkR180":820,"walkRRight":822,"walkRLeft":821,"standRRight":823,"standRLeft":823
 					return;
+				}
 			}
 		}
 
@@ -100,14 +105,14 @@ public class ExamplePlugin extends Plugin
 			started = true;
 		}
 		if (gameStateChanged.getGameState() == GameState.LOGIN_SCREEN && started) {
-			write(anims);
+			write(anims, "./item-animations.json");
 		}
 	}
 
-	void write(HashMap object) {
+	void write(HashMap object, String name) {
 		GsonBuilder builder = new GsonBuilder().disableHtmlEscaping();
-		Gson gson2 = builder.create();
-		try (OutputStream outputStream = new FileOutputStream("./item-animations.json")){
+		Gson gson2 = builder.setPrettyPrinting().create();
+		try (OutputStream outputStream = new FileOutputStream(name)){
 			BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.US_ASCII));
 			gson2.toJson(object.values(), bufferedWriter);
 			bufferedWriter.close();
